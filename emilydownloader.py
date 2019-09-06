a9c3b1eb97bc97997f06deeee7cce0e6355a9fd2
Fei <unshi@outlook.com>
2019-09-05 20:06:47 -0400

    update

--- emilydownloader.py
+++ emilydownloader.py
@@ -16,13 +16,13 @@ def getphoto():
         items=re.findall(pattern,html)
 #print(items)
         for item in items: 
-                photo_url='https://ftopx.com/images/' + str(item[0]) + '/ftopx.com_' + str(item[1])
+                photo_url='https://ftopx.com/images/' + str(item[0]) + '/ftop.ru_' + str(item[1])
                 print(photo_url)
                 print(photo_url,file=doc)
 # print(html)
 
 #getphoto()
-for i in range(1,pages):
+for i in range(11,pages):
         webpage="https://ftopx.com/tags/emily+bloom/page/"+str(i)+"/"
         driver.get(webpage)
         getphoto()
